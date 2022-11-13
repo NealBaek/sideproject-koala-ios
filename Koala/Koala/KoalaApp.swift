679026d896499e5d198d8198866bc695bbe6678e
NealBaek <bsy7359@gmail.com>
2022-11-13 17:46:59 +0900

    initial commit

---
+++ Koala/Koala/KoalaApp.swift
@@ -0,0 +1,20 @@
+//
+//  KoalaApp.swift
+//  Koala
+//
+//  Created by 백수호 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct KoalaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
