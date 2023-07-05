e3fdeb45ca9b4037719452e20aa7547b209085cf
nealbaek <bsy7359@gmail.com>
2023-07-06 08:16:08 +0900

    달력 화면 조정 작업

--- Koala/Koala/Calendar/CalendarView.swift
+++ Koala/Koala/Calendar/CalendarView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct CalendarView: View {
     
+    enum Constants {
+        static let calendarHeaderHeight: CGFloat = 50
+    }
+    
     @ObservedObject var viewModel = CalendarViewModel()
     
     private let calendarRowCount = 6
@@ -17,78 +21,78 @@ struct CalendarView: View {
     var body: some View {
 
         VStack(spacing: 0) {
-            HStack(spacing: 0){
-                
-                Spacer()
-                    .frame(width: 70)
-                
-                Spacer()
-                
-                Image(systemName: "arrow.left")
-                    .frame(width: 50)
-                    .onTapGesture { viewModel.moveToPreviousMonth() }
-                
-                if let selectedMonth = viewModel.selectedMonth {
-                    Text("\(selectedMonth.year).\(selectedMonth.month)")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.white)
-                }
-                
-                Image(systemName: "arrow.right")
-                    .frame(width: 50)
-                    .onTapGesture { viewModel.moveToNextMonth() }
-                
-                Spacer()
-                
-                Text("오늘")
-                    .font(.system(size: 20, weight: .bold))
-                    .foregroundColor(.white)
-                    .frame(width: 70)
-                    .onTapGesture { viewModel.moveToTodayMonth() }
-                
-            }
-            .frame(height: 50)
-            .background(Color.green)
             
-            GeometryReader { proxy in
+            calendarHeader
+                .frame(height: Constants.calendarHeaderHeight)
+                .background(Color.green)
                 
-                LazyHStack {
-                    TabView(selection: $viewModel.selectedMonthIdx) {
-                        // 월 단위 탭
-                        ForEach(viewModel.calendarData.monthList) { month in
-                            VStack {
-                                GeometryReader { proxy2 in
-                                    LazyVGrid(
-                                        columns: Array(repeating: .init(.flexible()), count: calendarColumnCount), spacing: 2) {
-                                            // 일 단위 탭
-                                            ForEach(month.dayList) { day in
-                                                
-                                                ZStack {
-                                                    day.isOutMonth ? Color.gray : Color.white
-                                                    Text("\(day.day)")
-                                                        .foregroundColor(day.isOutMonth ? Color.white : Color.black)
-                                                }
-                                                .id(day.id)
-                                                .frame(
-                                                    width: (proxy2.size.width) / CGFloat(calendarColumnCount),
-                                                    height: (proxy2.size.height) / CGFloat(calendarRowCount))
-                                            }
+            GeometryReader { proxy in
+                TabView(selection: $viewModel.selectedMonthIdx) {
+                    // 월 단위 탭
+                    ForEach(viewModel.calendarData.monthList) { month in
+
+                        let dayViewSpacing: CGFloat = 2
+                        
+                        LazyVGrid(
+                            columns: Array(repeating: .init(.flexible()), count: calendarColumnCount), spacing: dayViewSpacing) {
+                                // 일 단위 탭
+                                ForEach(month.dayList) { day in
+
+                                    ZStack {
+                                        day.isOutMonth ? Color.gray : Color.white
+                                        Text("\(day.day)")
+                                            .foregroundColor(day.isOutMonth ? Color.white : Color.black)
                                     }
+                                    .id(day.id)
+                                    .frame(
+                                        width: (proxy.size.width) / CGFloat(calendarColumnCount),
+                                        height: (proxy.size.height) / CGFloat(calendarRowCount))
                                 }
-                                Spacer()
-                            }
-                            .id(month.id)
-                            .frame(width: proxy.size.width, height: proxy.size.height)
                         }
+                        .id(month.id)
+                        .frame(height: proxy.size.height + CGFloat(calendarRowCount) * dayViewSpacing)
+                        .background(Color.gray.opacity(0.5))
                     }
-                    .frame(width: proxy.size.width, height: proxy.size.height)
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                }.background(Color.gray.opacity(0.5))
-            }.edgesIgnoringSafeArea(.all)
+                }
+                .background(Color.blue.opacity(0.5))
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            }
+            
         }
-        .navigationBarTitle("캘린더 테스트")
     }
     
+    var calendarHeader: some View {
+        HStack(spacing: 0){
+            
+            Spacer()
+                .frame(width: 70)
+            
+            Spacer()
+            
+            Image(systemName: "arrow.left")
+                .frame(width: 50)
+                .onTapGesture { viewModel.moveToPreviousMonth() }
+            
+            if let selectedMonth = viewModel.selectedMonth {
+                Text("\(selectedMonth.year).\(selectedMonth.month)")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+            }
+            
+            Image(systemName: "arrow.right")
+                .frame(width: 50)
+                .onTapGesture { viewModel.moveToNextMonth() }
+            
+            Spacer()
+            
+            Text("오늘")
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(.white)
+                .frame(width: 70)
+                .onTapGesture { viewModel.moveToTodayMonth() }
+            
+        }
+    }
 }
 
 struct CalendarView_Previews: PreviewProvider {
--- Koala/Koala/KoalaApp.swift
+++ Koala/Koala/KoalaApp.swift
@@ -13,53 +13,56 @@ struct KoalaApp: App {
 
     @State var selection = 0
     
+    let tabBarHeight: CGFloat = 70
+    
     init() {
         // 런치 스크린 타이머
         Thread.sleep(forTimeInterval: 1)
-        UITabBar.appearance().barTintColor = .green
     }
     
     var body: some Scene {
         WindowGroup {
-            VStack(spacing: 0) {
-                TabView(selection: $selection) {
-                    CalendarView()
-                        .tag(0)
-                    Text("요즘")
-                        .tag(1)
-                    Text("통계")
-                        .tag(2)
-                    Text("설정")
-                        .tag(3)
-                }
-                
-                GeometryReader { proxy in
+            GeometryReader { proxy in
+                VStack(spacing: 0) {
+                    TabView(selection: $selection) {
+                        CalendarView()
+                            .tag(0)
+                        Text("요즘")
+                            .tag(1)
+                        Text("통계")
+                            .tag(2)
+                        Text("설정")
+                            .tag(3)
+                    }.frame(height: proxy.size.height - tabBarHeight)
+                    
                     HStack(spacing: 0) {
+                        let tabItemWidth = proxy.size.width / 4
                         Text("기록")
                             .foregroundColor(selection == 0 ? .green : .gray)
                             .font(.system(size: 20))
-                            .frame(width: proxy.size.width / 4, height: proxy.size.height)
+                            .frame(width: tabItemWidth, height: tabBarHeight)
                             .onTapGesture { selection = 0 }
                         Text("요즘")
                             .foregroundColor(selection == 1 ? .green : .gray)
                             .font(.system(size: 20))
-                            .frame(width: proxy.size.width / 4, height: proxy.size.height)
+                            .frame(width: tabItemWidth, height: tabBarHeight)
                             .onTapGesture { selection = 1 }
                         Text("통계")
                             .foregroundColor(selection == 2 ? .green : .gray)
                             .font(.system(size: 20))
-                            .frame(width: proxy.size.width / 4, height: proxy.size.height)
+                            .frame(width: tabItemWidth, height: tabBarHeight)
                             .onTapGesture { selection = 2 }
                         Text("설정")
                             .foregroundColor(selection == 3 ? .green : .gray)
                             .font(.system(size: 20))
-                            .frame(width: proxy.size.width / 4, height: proxy.size.height)
+                            .frame(width: tabItemWidth, height: tabBarHeight)
                             .onTapGesture { selection = 3 }
                     }
+                    .frame(height: tabBarHeight)
+                    .background(Color.white)
                 }
-                .frame(height: 70)
-                .background(Color.white)
             }
+            
 //            ContentView()
 //                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
